7d44d9e119c58c9a050e7941aac5b167e7e5d38c
Subodhkhobragade <subodhkhobragade0@gmail.com>
2022-12-31 22:04:45 +0530

    my event file

---
+++ JavaScriptPractice/event.js
@@ -0,0 +1,13 @@
+function focusfunction(params) {
+    params.style.backgroundColor='pink';
+    
+}
+function blurfunction(params) {
+    params.style.backgroundColor='yellow'
+    
+}
+function key(params) {
+    let x=document.getElementById('upper')
+    x.value= x.value.toUpperCase();
+    x.value=x.style.textDecorationColor('pink');   
+}
